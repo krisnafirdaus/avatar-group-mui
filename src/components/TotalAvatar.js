cba90e6219bdec211b59f6852faa338596bd2e93
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-03-09 11:52:42 +0700

    initial project

---
+++ src/components/TotalAvatar.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+
+export default function TotalAvatars() {
+  return (
+    <AvatarGroup total={8}>
+      <Avatar
+        alt="Remy Sharp"
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+      />
+      <Avatar
+        alt="Travis Howard"
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+      />
+      <Avatar
+        alt="Agnes Walker"
+        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80"
+      />
+    </AvatarGroup>
+  );
+}
